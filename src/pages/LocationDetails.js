bf7906f01e2376147f92a99db7ae0f14b16f298b
Marcusdev97 <marcuschin.flp@.com>
2024-09-30 20:13:56 +0800

    Location Details Update Display

--- src/pages/LocationDetails.js
+++ src/pages/LocationDetails.js
@@ -2,50 +2,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/LocationDetails.css';
-import sampleImage from '../images/600x400.png';
+import ss15Image from '../images/ss15-condo.jpg';
+import ss13Image from '../images/ss13-condo.png';
+import casaImage from '../images/casatiara-condo.jpg';
+import greenfieldImage from '../images/greenfield-condo.jpg';
+import geolakeImage from '../images/geolake-condo.jpg';
 
 function LocationDetails() {
   // Static location data including image, name, and description
   const locations = [
     {
       name: 'SS15',
-      description: 'SS15 是一个著名的学生聚集区，靠近购物中心和餐馆，生活便利，交通方便。',
-      image: sampleImage,
+      description:
+        'SS15 是泰莱大学校车途径小区，距离学校直线距离约为5.1km，驾车约为12分钟可达。小区24h安保，配套游泳池健身房篮球场等。因其地处小吃街，楼下各国美食应有尽有。附近学校有：英迪大学，亚洲大学，双威大学等。附近医院有：梳邦医疗中心。',
+      image: ss15Image,
     },
     {
       name: 'SS13',
-      description: 'SS13 靠近多个大学，环境安静，非常适合学生和工作人士。',
-      image: sampleImage,
+      description:
+        'SS13 与SS15同属一个开发商，小区建面风格类似，是SS15隔壁街区。非泰莱大学校车途径，距离莫纳什大学较近，可步行至莫纳什大学。小区配套游泳池健身房，楼下有本地餐馆以及便利店，生活便利程度完全可以保障。',
+      image: ss13Image,
     },
     {
       name: 'Casa Tiara',
-      description: 'Casa Tiara 是一个非常受欢迎的公寓，设施完善，房租合理。',
-      image: sampleImage,
-    },
-    {
-      name: 'Icon City',
-      description: 'Icon City 是一个现代化的居住区，配有高端设施和购物中心。',
-      image: sampleImage,
+      description:
+        'Casa Tiara 可乘坐校车直达泰莱大学，直线距离约为5.9km，驾车约为14分钟。户型多为三居室和两居室，整体价格是泰莱大学周边最划算的。步行10分钟可达BIG商场和超市，周边小区密集，泰莱同学许多都会选择居住这里。',
+      image: casaImage,
     },
     {
       name: 'Greenfield',
-      description: 'Greenfield 提供宽敞的居住空间，周围环境绿意盎然，非常适合家庭居住。',
-      image: sampleImage,
-    },
-    {
-      name: 'Geosense',
-      description: 'Geosense 是一个非常安静和安全的居住区，靠近主要大学。',
-      image: sampleImage,
+      description:
+        'Greenfield 是泰莱大学校车途径小区，距离学校直线距离约为3.6km，驾车约为10分钟可达。小区24h安保，配套游泳池健身房学习室等。附近生活便利，距离双威金字塔商场仅2km，楼下有各类中餐馆以及杂货店等。附近学校有：双威国际学校、莫纳什大学、双威大学等。附近医院有：双威医疗中心。',
+      image: greenfieldImage,
     },
     {
       name: 'Geolake',
-      description: 'Geolake 是一个湖景社区，景色优美，生活设施齐全。',
-      image: sampleImage,
-    },
-    {
-      name: 'Union & Edumetro',
-      description: 'Union 和 Edumetro 是现代化公寓楼，提供完善的设施，特别适合学生。',
-      image: sampleImage,
+      description:
+        'Geolake 是同为Sunway开发商旗下的高端小区，其地理位置紧邻彼此，出小区门后就是Sunway Avenue商场，也可经此通过人行天桥直达泰莱大学。小区配套设施豪华，无边游泳池健身房学习室以及小区内绿化公园等，安保系数也是最高等级。',
+      image: geolakeImage,
     },
   ];
 
