c1e59590565e06740a71b059039652160c81a300
Marcusdev97 <marcuschin.flp@.com>
2024-10-10 15:09:44 +0800

    Upload FORCE to HTTPS

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,13 @@
 // src/utils/api.js
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL;
+console.log(API_URL);
+
 export const fetchProperties = async () => {
   try {
-    const response = await axios.get('/api/properties');
+    // 使用环境变量中的 API URL
+    const response = await axios.get(`${API_URL}/api/properties`);
     return response.data;
   } catch (error) {
     console.error('Error fetching properties:', error);
