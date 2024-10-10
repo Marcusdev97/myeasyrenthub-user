fce37fe7014c68c3540e91645053231503921437
Marcusdev97 <marcuschin.flp@.com>
2024-10-10 14:34:17 +0800

    Serverless Update Function To Connect

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,9 @@
 // src/utils/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
 export const fetchProperties = async () => {
   try {
-    const response = await axios.get(`${API_URL}/api/properties`);
+    const response = await axios.get('/api/properties');
     return response.data;
   } catch (error) {
     console.error('Error fetching properties:', error);
